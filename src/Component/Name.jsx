b471803a8841ed7203b9cd0cba264f001b1fe0eb
Ashwin <ashwin.angadi1@gmail.com>
2024-01-01 23:22:52 +0530

    name update

--- src/Component/Name.jsx
+++ src/Component/Name.jsx
@@ -9,10 +9,13 @@ export const Name = () => {
                     <h1>Hi, my name is</h1> 
                 </div>
                 <div  >
-                    <h2 className=" text-3xl md:text-7xl font-sans text-lightest-slate font-bold ">Ashwin Angadi.</h2>
+                    <h2 className=" text-[clamp(30px,10vw,72px)] leading-none flex flex-nowrap gap-4 font-sans text-lightest-slate font-bold ">
+                        <span>Ashwin</span>
+                        <span> Angadi.</span>
+                        </h2>
                 </div>
                 <div>
-                    <h3 className="text-3xl md:text-7xl font-sans text-slate font-bold mt-2">I build things for the web.</h3>
+                    <h3 className="text-[clamp(30px,10vw,72px)] leading-none font-sans text-slate font-bold mt-2">I build things for the web.</h3>
                 </div>
             </div>
         </section>
